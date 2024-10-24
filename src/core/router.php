bf5ce6a84a1e8c9374cf5c62b90cd1aeab9258ba
Denilson Jhon <denilsontrillo@gmail.com>
2024-10-23 22:52:01 -0600

    Configuracion de las rutas de la api de login, y ejemplo de middleware .

---
+++ src/core/router.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/../middleware/login.php';
+
+$router = new \Bramus\Router\Router();
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../config/');
+$dotenv->load();
+
+
+$router->get('/', function () {
+    echo 'principal';    
+});
+
+// Aplicar middleware a la ruta protegida, Ejemplo
+// $router->before('GET', '/protected', 'authMiddleware');
+
+// $router->get('/protected', function() {
+//     echo "Bienvenido a la sección protegida.";
+// });
+
+# Api de login
+$router->mount('/login', function () use ($router) {
+ 
+    # Register, registra el usuario
+    $router->post('/register', function () {
+        echo 'register';
+    });
+
+    # Authenticate, valida el usuario y password
+    $router->get('/authenticate', function () {
+        // session_start();
+        // $_SESSION['user'] = 'usuario'; // Simulación de inicio de sesión
+        echo "Has iniciado sesión. Ahora puedes acceder a la sección protegida.";
+    });
+
+    # Logout, cierra la sesion
+    $router->get('/logout', function () {
+        // session_destroy();
+        echo 'logout';
+    });
+});
+
+$router->run();
