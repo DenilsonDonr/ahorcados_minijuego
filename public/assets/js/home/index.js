55c1ab27aaa9ea15e4498cba1914af9b0d42dcb7
Denilson Jhon <denilsontrillo@gmail.com>
2024-10-24 22:46:14 -0600

    se creo una funcion para obtener la palabra

---
+++ public/assets/js/home/index.js
@@ -0,0 +1,11 @@
+// Obtenemos una palabra aleatoria de la API y la mostramos en el DOM
+const getWord = async () => {
+    let response = await FetchData('http://localhost/ahorcados_minijuego/word/getWord', 'GET', {});
+    
+    if(!response)
+    {
+        alert('No se pudo obtener la palabra', response);
+    }
+
+    console.log(response);
+};
