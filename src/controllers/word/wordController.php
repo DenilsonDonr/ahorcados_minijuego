84d8da1df1a3398881705d34aae59bba691a0ccb
Denilson Jhon <denilsontrillo@gmail.com>
2024-10-24 22:47:21 -0600

    creamos del controlador, por ahora obtenimos la palabra e instanciamos el repositorio inyectando la DB

--- src/controllers/word/wordController.php
+++ src/controllers/word/wordController.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace Jay\controllers\word;
+
+use Jay\core\Database;
+use Jay\repositories\wordRepository\WordRepository;
+
+class WordController
+{
+    private $wordRepository;
+
+    private $Model;
+
+    public function __construct()
+    {
+        // instanciamos el repositorio
+        $this->Model = Database::getInstance();
+        $this->wordRepository = new WordRepository($this->Model->getConnection());
+    }
+
+    public function getWord()
+    {
+        // Obtenemos una palabra aleatoria desde el repositorio
+        $word = $this->wordRepository->getRandomWord();
+
+        echo json_encode(['message' => $word]);
+    }
+}
