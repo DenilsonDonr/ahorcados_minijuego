1afbfec7c986d8b364718567e546beabd969e45e
Denilson Jhon <denilsontrillo@gmail.com>
2024-10-26 19:24:53 -0600

    Se agrego un documentLoaded, y tambien se manejo el evento del formulario , evitamos su evento por defecto y creamos el objeto, se lo enviamos a la funcion de postRegister, el cual incluye un fetch , enviando los datos  y retornado una respuesta.

---
+++ public/assets/js/home/login.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Obtenemos el DOM del formulario de registro
+    const registerForm = document.getElementById("registerForm");
+    
+    registerForm.addEventListener("submit", (e) => {
+        e.preventDefault();  // Detenemos la recarga de la página
+        
+        // Obtenemos los datos del formulario directamente
+        const data = {
+            user: registerForm.user.value,
+            email: registerForm.email.value,
+            password: registerForm.password.value
+        };
+        
+        // Validamos que todos los campos estén completos
+        if (!data.user || !data.email || !data.password || !registerForm.passwordConfirmed.value) {
+            alert('Todos los campos son obligatorios');
+            return;
+        }
+    
+        // Validamos que la contraseña tenga al menos 8 caracteres
+        // if (data.contrasena.length < 8) {
+        //     alert('La contraseña debe tener al menos 8 caracteres');
+        // }
+        // Validamos que la confirmación de contraseña coincida
+        // if (data.password !== registerForm.passwordConfirmed.value) {
+        //     alert('Las contraseñas no coinciden');
+        //     return;
+        // }
+    
+        // Enviar datos a la API
+        postRegister(data);
+    });
+
+    // Función para registrar un usuario
+    const postRegister = async (data) => {
+        try {
+            let response = await FetchData('http://localhost/ahorcados_minijuego/usuario/login/register', 'POST', data);
+    
+            if (!response || !response.success || response.error) {
+                alert('Error en el registro: ' + response.error);
+                return;
+            }
+    
+            alert("Usuario: " + response.success);
+    
+            cleanForm();
+
+            modalContent.classList.add("close-animation");
+            setTimeout(function () {
+                modal.style.display = "none"; // Ocultar el modal despues de que la animacion termine
+            }, 1800); // Tiempo igual al de la animacion (1.8s)
+        } catch (error) {
+            console.error("Error en el registro:", error);
+        }
+    }
+    
+    // Función para limpiar el formulario de registro
+    const cleanForm = () => {
+        registerForm.reset();
+    }
+
+});
