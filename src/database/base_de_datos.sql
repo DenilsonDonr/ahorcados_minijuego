2a2d5b3729aee40be114afb132203955e97a98aa
Denilson Jhon <denilsontrillo@gmail.com>
2024-11-09 21:08:24 -0600

    Actualizacion de DB, en codigo.

--- src/database/base_de_datos.sql
+++ src/database/base_de_datos.sql
@@ -1,193 +1,86 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: ahorcados_senati
--- ------------------------------------------------------
--- Server version	8.0.30
+create database ahorcados_senati;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+use ahorcados_senati;
 
---
--- Table structure for table `jugadas`
---
+-- usuarios
+CREATE TABLE usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    usuario VARCHAR(255) NOT NULL,
+    edad CHAR(3) NOT NULL,
+    correo VARCHAR(255) NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
+    tipo_usuario CHAR(1) NOT NULL,
+    estado CHAR(1) NOT NULL
+);
 
-DROP TABLE IF EXISTS `jugadas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `jugadas` (
-  `id_jugada` int NOT NULL AUTO_INCREMENT,
-  `intento` smallint NOT NULL DEFAULT '0',
-  `fecha_inicio` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `hora` time NOT NULL,
-  `estado` int DEFAULT NULL,
-  `id_palabra` int NOT NULL,
-  `id_usuario` int NOT NULL,
-  PRIMARY KEY (`id_jugada`),
-  KEY `id_palabra_fk_jugadas` (`id_palabra`),
-  KEY `id_usuario_fk_jugadas` (`id_usuario`),
-  CONSTRAINT `id_palabra_fk_jugadas` FOREIGN KEY (`id_palabra`) REFERENCES `palabras` (`id_palabra`),
-  CONSTRAINT `id_usuario_fk_jugadas` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+INSERT INTO usuarios (usuario, edad, correo, contrasena, tipo_usuario, estado) 
+VALUES (
+    'Juan',
+    '25', 
+    'juan.perez@example.com', 
+    'hashed_password',  -- Suponiendo que la contraseña esté hasheada
+    1,  -- Tipo de usuario ('A' puede ser Administrador, por ejemplo)
+    1  -- Estado (puede ser 'A' para activo, 'I' para inactivo)
+);
 
---
--- Dumping data for table `jugadas`
---
 
-LOCK TABLES `jugadas` WRITE;
-/*!40000 ALTER TABLE `jugadas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `jugadas` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `paginas`
---
+-- roles
+CREATE TABLE roles (
+    id_rol INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    estado CHAR(1) NOT NULL
+);
 
-DROP TABLE IF EXISTS `paginas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `paginas` (
-  `id_pagina` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `estado` char(1) NOT NULL,
-  PRIMARY KEY (`id_pagina`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- paginas
+CREATE TABLE paginas (
+    id_pagina INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    estado CHAR(1) NOT NULL
+);
 
---
--- Dumping data for table `paginas`
---
+-- permisos
+CREATE TABLE permisos (
+    id_permiso INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id_rol INT NOT NULL,
+    id_pagina INT NOT NULL,
+    CONSTRAINT id_rol_fk FOREIGN KEY (id_rol) REFERENCES roles(id_rol),
+    CONSTRAINT id_pagina_fk FOREIGN KEY (id_pagina) REFERENCES paginas(id_pagina)
+);
 
-LOCK TABLES `paginas` WRITE;
-/*!40000 ALTER TABLE `paginas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `paginas` ENABLE KEYS */;
-UNLOCK TABLES;
+-- palabras
+CREATE TABLE palabras (
+    id_palabra INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    palabra VARCHAR(255) NOT NULL,
+    json_images JSON NOT NULL,
+    niveles INT NOT NULL, 
+    id_usuario INT NOT NULL,
+    estado CHAR(1) NOT NULL,
+    CONSTRAINT id_usuario_fk_palabras FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
 
---
--- Table structure for table `palabras`
---
+INSERT INTO palabras (palabra, json_images, niveles, id_usuario, estado) 
+VALUES (
+    'bicicleta', 
+    '["b_c1_ds.webp", "b_c2_sd.jpg", "b_c3_sd.jpg", "b_c4_sd.webp"]',  -- JSON array de imágenes
+    1,  -- Nivel de dificultad o número de niveles
+    1,  -- ID de usuario (asumiendo que este usuario ya existe en la tabla `usuarios`)
+    '1' -- Estado (puede ser 'A' para activo, por ejemplo)
+);
+drop table palabras;
 
-DROP TABLE IF EXISTS `palabras`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `palabras` (
-  `id_palabra` int NOT NULL AUTO_INCREMENT,
-  `palabra` varchar(255) NOT NULL,
-  `json_images` json NOT NULL,
-  `niveles` int NOT NULL,
-  `id_usuario` int NOT NULL,
-  `estado` char(1) NOT NULL,
-  PRIMARY KEY (`id_palabra`),
-  KEY `id_usuario_fk_palabras` (`id_usuario`),
-  CONSTRAINT `id_usuario_fk_palabras` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- jugadas
+CREATE TABLE jugadas(
+    id_jugada INT PRIMARY KEY AUTO_INCREMENT,
+    intento SMALLINT NOT NULL DEFAULT 0,
+    fecha_inicio DATETIME NOT NULL DEFAULT NOW(),
+    hora TIME NOT NULL,
+    estado INT,
+    id_palabra INT NOT NULL,
+    id_usuario INT NOT NULL,
+    CONSTRAINT id_palabra_fk_jugadas FOREIGN KEY (id_palabra) REFERENCES palabras(id_palabra),
+    CONSTRAINT id_usuario_fk_jugadas FOREIGN KEY (id_usuario) REFERENCES usuarios(id)
+);
 
---
--- Dumping data for table `palabras`
---
 
-LOCK TABLES `palabras` WRITE;
-/*!40000 ALTER TABLE `palabras` DISABLE KEYS */;
-/*!40000 ALTER TABLE `palabras` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `permisos`
---
-
-DROP TABLE IF EXISTS `permisos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `permisos` (
-  `id_permiso` int NOT NULL AUTO_INCREMENT,
-  `id_rol` int NOT NULL,
-  `id_pagina` int NOT NULL,
-  PRIMARY KEY (`id_permiso`),
-  KEY `id_rol_fk` (`id_rol`),
-  KEY `id_pagina_fk` (`id_pagina`),
-  CONSTRAINT `id_pagina_fk` FOREIGN KEY (`id_pagina`) REFERENCES `paginas` (`id_pagina`),
-  CONSTRAINT `id_rol_fk` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `permisos`
---
-
-LOCK TABLES `permisos` WRITE;
-/*!40000 ALTER TABLE `permisos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `permisos` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `roles`
---
-
-DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `roles` (
-  `id_rol` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `estado` char(1) NOT NULL,
-  PRIMARY KEY (`id_rol`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `usuarios`
---
-
-DROP TABLE IF EXISTS `usuarios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `usuarios` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombres` varchar(255) NOT NULL,
-  `apellidos` varchar(255) NOT NULL,
-  `edad` char(3) NOT NULL,
-  `correo` varchar(255) NOT NULL,
-  `contraseña` varchar(255) NOT NULL,
-  `tipo_usuario` char(1) NOT NULL,
-  `estado` char(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuarios`
---
-
-LOCK TABLES `usuarios` WRITE;
-/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-10-23 21:25:06
+SELECT palabra, json_images FROM palabras ORDER BY RAND() LIMIT 1
