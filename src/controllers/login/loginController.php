2340200e226be6cc26498661a346ca1167327ff7
dgmanyute <manuel_sp_10@hotmail.com>
2024-11-11 23:00:32 -0500

    se crea la funcion login user

--- src/controllers/login/loginController.php
+++ src/controllers/login/loginController.php
@@ -41,4 +41,29 @@ class LoginController
         }
         echo json_encode(['success' => '¡Registro exitoso! Bienvenido']);
     }
+
+    public function loginUser()
+{
+    // Leer el cuerpo de la solicitud
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
+
+    $username = $data['user'];
+    $password = $data['password'];
+
+    $user = $this->userRepository->loginUser($username, $password);
+
+    if ($user) {
+        // Iniciar la sesión y almacenar los datos del usuario
+        $session = new \Jay\core\Session();
+        $session->setUser($user);
+
+        echo json_encode(['message' => 'Login exitoso', 'user' => $user]);
+    } else {
+        echo json_encode(['message' => 'Credenciales inválidas']);
+    }
+}
+
+
+
 }
