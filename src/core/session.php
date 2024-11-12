7685cacbcb8604c216d360186409849bb679d36c
dgmanyute <manuel_sp_10@hotmail.com>
2024-11-11 23:06:18 -0500

    se crea el metodo recuperar datos usuario ,método cierra la sesión del usuario.método cierra la sesión del usuario.

--- src/core/session.php
+++ src/core/session.php
@@ -10,4 +10,20 @@ class Session
             session_start();
         }
     }
+
+    public function setUser($user)
+    {
+        $_SESSION['user'] = $user;
+    }
+
+    public function getUser()
+    {
+        return isset($_SESSION['user']) ? $_SESSION['user'] : null;
+    }
+
+    public function logout()
+    {
+        session_unset();
+        session_destroy();
+    }
 }
