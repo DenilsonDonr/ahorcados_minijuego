87aafad18625e790b564d550277a57440f92fb6d
dgmanyute <manuel_sp_10@hotmail.com>
2024-11-11 23:00:48 -0500

    se crea un rutas pero faalta verificar

--- src/core/router.php
+++ src/core/router.php
@@ -32,19 +32,43 @@ $router->mount('/usuario/login', function () use ($router) {
         $user->registerUser();
     });
 
-    # Authenticate, valida el usuario y password
-    $router->get('/authenticate', function () {
-        // session_start();
-        // $_SESSION['user'] = 'usuario'; // Simulación de inicio de sesión
-        echo "Has iniciado sesión. Ahora puedes acceder a la sección protegida.";
+    # Authenticate, valida el usuario y password por post
+    $router->post('/authenticate', function () {
+        $user = new LoginController();
+        $user->loginUser();
     });
 
-    # Logout, cierra la sesion
-    $router->get('/logout', function () {
-        // session_destroy();
-        echo 'logout';
+    $router->post('/logout', function () {
+        $session = new \Jay\core\Session();
+        $session->logout();
+        echo json_encode(['success' => true, 'message' => 'Sesión cerrada exitosamente']);
     });
+    
+    
+
+    // # Authenticate, valida el usuario y password
+    // $router->get('/authenticate', function () {
+    //     // session_start();
+    //     // $_SESSION['user'] = 'usuario'; // Simulación de inicio de sesión
+    //     echo "Has iniciado sesión. Ahora puedes acceder a la sección protegida.";
+    // });
+
+    // # Logout, cierra la sesion
+    // $router->get('/logout', function () {
+    //     session_destroy();
+    //     echo json_encode(['success' => true, 'message' => 'Sesión cerrada exitosamente2']);
+    // });
 });
+
+$router->get('/usuario/session', function () {
+    session_start();
+    if (isset($_SESSION['user'])) {
+        echo json_encode(['isLoggedIn' => true, 'user' => $_SESSION['user']]);
+    } else {
+        echo json_encode(['isLoggedIn' => false]);
+    }
+});
+
 
 # Rutas de word
 
