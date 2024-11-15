0c99d64bdf161fabf84bb93cca5129a9d6cd12e8
dgmanyute <manuel_sp_10@hotmail.com>
2024-11-15 16:11:27 -0500

    esto ya esta  subido pero para poder suir  lo tengo que subir

--- src/repositories/wordRepository/wordRepository.php
+++ src/repositories/wordRepository/wordRepository.php
@@ -17,7 +17,7 @@ class WordRepository implements WordInterface {
     // Agregar usuario a la base de datos
     public function getRandomWord() {
         // Seleccionamos la palabra y el JSON de las imágenes
-        $sql = "SELECT palabra, json_images FROM palabras ORDER BY RAND() LIMIT 1";
+        $sql = "SELECT id_palabra, palabra, json_images FROM palabras ORDER BY RAND() LIMIT 1";
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
     
@@ -30,11 +30,28 @@ class WordRepository implements WordInterface {
         // Decodificamos el JSON en un array
         $imagenes = json_decode($row['json_images'], true); // true convierte el JSON en array
     
+        // Extraemos el ID de la palabra
+        $idPalabra = $row['id_palabra'];
+
         // Retornamos la palabra y el array de nombres de las imágenes
         return [
             'palabra' => $palabra,
-            'imagenes' => $imagenes // Array de nombres de las imágenes
-        ];
+            'imagenes' => $imagenes, // Array de nombres de las imágenes
+            'id_palabra' => $idPalabra
+        ]; 
     }
-    
+    // Método estático que devuelve el número máximo de palabras en la base de datos
+    public function getMaximumNumberOfRows()
+    {
+        // Usamos la misma conexión de base de datos que se pasa como argumento
+        $sql = "SELECT COUNT(id_palabra) as total FROM palabras limit 1";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+
+        // Obtenemos el resultado
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Devolvemos el número total de palabras
+        return $row['total'];
+    }
 }
